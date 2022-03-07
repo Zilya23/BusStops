45bc27f69a2ae822d7d21c486103690613decae9
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-03-07 10:20:23 +0300

    Создала новые словари и метод добавления автобуса со всеми остановками, но пока не работает

--- BusStops/Program.cs
+++ BusStops/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Data;
+using System.Collections.Generic;
 
 namespace BusStops
 {
-    class Program
+    public class Program
     {
+        static public Dictionary<string, HashSet<string>> Buses;
+        static public Dictionary<string, HashSet<string>> Stops;
+        static public HashSet<string> Stop;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Buses = new Dictionary<string, HashSet<string>>();
+            Stops = new Dictionary<string, HashSet<string>>();
+
+            //Dictionary<string, HashSet<string>> Buses = new Dictionary<string, HashSet<string>>();
+            //Dictionary<string, HashSet<string>> Stops = new Dictionary<string, HashSet<string>>();
+
+            List<string> Stop_List = new List<string>() { "Остановка1", "Остановка2", "Остановка3", "Остановка4" };
+
+            NEW_HASH(Stop_List);
+            NEW_BUS(5, "34", Stop);
+            Print_Bus(Buses);
+        }
+
+
+        public static void NEW_BUS(int count, string busName, HashSet<string>Stop)
+        {
+            Buses.TryAdd(busName, Stop);
+        }
+
+        public static void NEW_HASH(List<string> Stop_List)
+        {
+            Stop_List.ForEach(delegate (string name)
+            {
+                Stop.Add(name);
+            });
+        }
+
+        public static void Print_Bus(Dictionary<string, HashSet<string>> Bus)
+        {
+
+            foreach (var i in Bus)
+            {
+                Console.WriteLine($"key: {i.Key}  value: {i.Value}");
+            }
         }
     }
 }

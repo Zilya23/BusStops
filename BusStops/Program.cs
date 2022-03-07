90849c4557f2eb5cc45dfaeb85ed42131a518870
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-03-07 11:16:33 +0300

    Сделала вывод одного или всех автобусов

--- BusStops/Program.cs
+++ BusStops/Program.cs
@@ -16,14 +16,17 @@ namespace BusStops
             Buses = new Dictionary<string, HashSet<string>>();
             Stops = new Dictionary<string, HashSet<string>>();
 
-            //Dictionary<string, HashSet<string>> Buses = new Dictionary<string, HashSet<string>>();
-            //Dictionary<string, HashSet<string>> Stops = new Dictionary<string, HashSet<string>>();
-
             List<string> Stop_List = new List<string>() { "Остановка1", "Остановка2", "Остановка3", "Остановка4" };
 
             NEW_HASH(Stop_List);
             NEW_BUS(5, "34", Stop);
             Print_Bus(Buses, "34");
+
+            NEW_HASH(Stop_List);
+            NEW_BUS(5, "45", Stop);
+            Print_Bus(Buses, "45");
+
+            Print_Buses(Buses);
         }
 
 
@@ -42,8 +45,23 @@ namespace BusStops
 
         public static void Print_Bus(Dictionary<string, HashSet<string>> Bus, string key)
         {
-            //Console.WriteLine($"Номер автобуса: {Bus.ContainsKey(key)}");
 
+            foreach (var i in Bus)
+            {
+                if(i.Key == key)
+                {
+                    Console.WriteLine($"Номер автобуса: {i.Key}");
+                    Console.WriteLine("Остановки: ");
+                    foreach (var a in i.Value)
+                    {
+                        Console.WriteLine(a);
+                    }
+                }
+            }
+        }
+
+        public static void Print_Buses(Dictionary<string, HashSet<string>> Bus)
+        {
             foreach (var i in Bus)
             {
                 Console.WriteLine($"Номер автобуса: {i.Key}");

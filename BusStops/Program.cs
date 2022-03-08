d6114b4d72a4a086746c6a7fb9336c791fb5d81d
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-03-09 01:37:58 +0300

    Короче ошибка в том, что все данные записываются в один хэшсет и из-за  этого данные одинаковые для всех автобусов, а как хранить все в разных я не знаю

--- BusStops/Program.cs
+++ BusStops/Program.cs
@@ -13,7 +13,7 @@ namespace BusStops
         static public HashSet<string> Stop = new HashSet<string>();
         static public HashSet<string> Bus = new HashSet<string>();
         static public List<string> Stop_List;
-        static void Main(string[] args)
+        static public void Main(string[] args)
         {
             Buses = new Dictionary<string, HashSet<string>>();
             Stops = new Dictionary<string, HashSet<string>>();
@@ -38,12 +38,12 @@ namespace BusStops
         }
 
 
-        public static void NEW_BUS(int count, string busName, HashSet<string>Stop)
+        public static void NEW_BUS(int count, string busName, HashSet<string> Stop)
         {
+            Stop.Clear();
             NEW_HASH(Stop_List, busName);
             Buses.TryAdd(busName, Stop);
         }
-
         public static void NEW_HASH(List<string> Stop_List, string bus_name)
         {
             Stop_List.ForEach(delegate (string name)
@@ -53,7 +53,6 @@ namespace BusStops
                 Stop.Add(name);
             });
         }
-
         public static void BUSES_FOR_STOP(Dictionary<string, HashSet<string>> Stopp, string _stop)
         {
             foreach (var i in Stopp)
@@ -70,7 +69,6 @@ namespace BusStops
                 }
             }
         }
-        
         public static void STOPS_FOR_BUS(string bus, Dictionary<string, HashSet<string>> Buss, Dictionary<string, HashSet<string>> Stopp)
         {
             foreach (var _bus in Buss)
@@ -98,23 +96,6 @@ namespace BusStops
                 }
             }
         }
-
-        public static void Print_Bus(Dictionary<string, HashSet<string>> Buss, string key)
-        {
-            foreach (var i in Buss)
-            {
-                if(i.Key == key)
-                {
-                    Console.WriteLine($"Номер автобуса: {i.Key}");
-                    Console.WriteLine("Остановки: ");
-                    foreach (var a in i.Value)
-                    {
-                        Console.WriteLine(a);
-                    }
-                }
-            }
-        }
-
         public static void ALL_BUSES(Dictionary<string, HashSet<string>> Buss)
         {
             foreach (var i in Buss)
@@ -124,6 +105,21 @@ namespace BusStops
                 foreach (var a in i.Value)
                 {
                     Console.WriteLine(a);
+                }
+            }
+        }
+        public static void Print_Bus(Dictionary<string, HashSet<string>> Buss, string key)
+        {
+            foreach (var i in Buss)
+            {
+                if (i.Key == key)
+                {
+                    Console.WriteLine($"Номер автобуса: {i.Key}");
+                    Console.WriteLine("Остановки: ");
+                    foreach (var a in i.Value)
+                    {
+                        Console.WriteLine(a);
+                    }
                 }
             }
         }

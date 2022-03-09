4ce1301376c1ef2666dec593a867adfab96a3586
kzlp-ldz <kamilyazakirova15935700@gmail.com>
2022-03-09 10:20:16 +0300

    trying to codding test

---
+++ BusStopsTest/BusStopsTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Data;
+using System.Collections.Generic;
+
+namespace BusStopsTest
+{
+    [TestClass]
+    public class BusStopsTest
+    {
+        [TestMethod]
+        public void TestAddingBus()
+        {
+            BusStops.Program.Buses = new Dictionary<string, HashSet<string>>();
+            BusStops.Program.Buses.TryAdd("34",);
+        }
+    }
+}

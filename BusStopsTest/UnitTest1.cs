0f4db35a6ad54b58661e88f1d380a086cd24129b
kzlp-ldz <kamilyazakirova15935700@gmail.com>
2022-03-09 09:40:42 +0300

    add unittest without anything

---
+++ BusStopsTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusStopsTest
+{
+    [TestClass]
+    public class BusStopsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
